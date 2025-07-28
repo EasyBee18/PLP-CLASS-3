47c0db1736a121764f2dd9c7d31bf8906f7b0532
hanofi-easybee <hanofialabi18@gmail.com>
2025-07-28 10:46:38 +0100

    check for odd and even numbers from users input

--- leapYear.py
+++ leapYear.py
@@ -5,3 +5,5 @@ if year %4 == 0:
     print("its a leap year!")
 else:
     print("its not a leap year")
+
+
---
+++ odd-evenNumber.py
@@ -0,0 +1,5 @@
+user_Number = int(input("Enter any number of your choice to check if its Even or Odd Number "))
+if user_Number %2 ==0:
+    print("The number you entered is an even number")
+else:
+    print("The number you entered is odd number")
